872cb08358595ede70b916f1b33a2be8e2a7cbf8
ashokkumarga <ashokkumarga0@gmail.com>
2022-12-18 11:09:39 +0530

    SOD

--- Practice/Firstprogram.java
+++ Practice/Firstprogram.java
@@ -1,5 +1,7 @@
 class Firstprogram{
     public static  void main(String args[]){
+       System.out.print("Hi nana");  //
+       System.out.print("Hi nana\n");
        System.out.println("Hi nana");
     }
     
