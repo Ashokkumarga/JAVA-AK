c7cfcdd3e1ce3495be36dab9222c93216f6be164
ashokkumarga <ashokkumarga0@gmail.com>
2022-12-18 17:25:50 +0530

    EOD

--- Practice/ConditionalStatements.java
+++ Practice/ConditionalStatements.java
@@ -1,5 +1,14 @@
-public ConditionalStatements{
-    public static void main(String[]args){
-        
+import java.util.*;
+
+public class ConditionalStatements{
+    public static void main(String args []){
+       Scanner sc = new Scanner(System.in);
+       int age = sc.nextInt();
+       
+       if(age >18){
+        System.out.println("Adult");
+       }else{
+        System.out.println("Not an Adult");
+       }
     }
 }
