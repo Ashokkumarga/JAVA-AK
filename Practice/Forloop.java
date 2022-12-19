b120a45d758adce5d39cb5c11eeb740459feb36e
ashokkumarga <ashokkumarga0@gmail.com>
2022-12-19 15:50:54 +0530

    Tables

---
+++ Practice/Forloop.java
@@ -0,0 +1,19 @@
+public class Forloop {
+
+  public static void main(String[] args) {
+    //System.out.println("Hi Bujji Kanna");
+    // For Loop
+    // for(initialisation ; condition ; updation)
+    // for ( int counter = 0 ; counter < 3 ; counter ++ ){
+    //     System.out.println("For Loop");
+    // }
+
+    // for ( int counter = 0 ; counter < 11 ; counter ++){
+    //     System.out.println(counter);
+    // }
+
+    for (int i = 0; i < 11; i++) {
+      System.out.println(i);
+    }
+  }
+}
--- Practice/Switch.java
+++ Practice/Switch.java
@@ -5,21 +5,38 @@ public class Switch {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int button = sc.nextInt();
-
-    if(button == 1){
+    switch (button){
+        case 1:
         System.out.println("Hello");
-    }else{
-        if(button == 2){
-            System.out.println("Namaste");
-        }else{
-            if(button == 3){
-                System.out.println("Bonjour");
-            }else{
-                System.out.println("Invalid Button");
-            }
-        }
+        break;
+
+        case 2:
+        System.out.println("Namste");
+        break;
+
+        case 3:
+        System.out.println("Namste");
+        break;
+
+        default :
+        System.out.println("Invalid button");
     }
   }
 
   
 }
+
+
+// if(button == 1){
+//     System.out.println("Hello");
+// }else{
+//     if(button == 2){
+//         System.out.println("Namaste");
+//     }else{
+//         if(button == 3){
+//             System.out.println("Bonjour");
+//         }else{
+//             System.out.println("Invalid Button");
+//         }
+//     }
+// }
