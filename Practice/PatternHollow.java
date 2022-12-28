04fe7c4affe7219b97ffe81bbb840b3866a21583
ashokkumarga <ashokkumarga0@gmail.com>
2022-12-28 19:59:02 +0530

    Dec28

--- Practice/PatternHollow.java
+++ Practice/PatternHollow.java
@@ -1,3 +1,13 @@
 public class PatternHollow {
-    for
+
+  public static void main(String[] args) {
+    int n = 3;
+    int m=15;
+    for (int i = 1; i <= n; i++) {
+        for(int j=1;j<=m;j++){
+           System.out.print("*");
+        }
+      System.out.println();
+    }
+  }
 }
