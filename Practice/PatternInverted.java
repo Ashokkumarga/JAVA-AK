b1b1c2146c16a6f23bb6f357c0ff94c093b29681
ashokkumarga <ashokkumarga0@gmail.com>
2022-12-29 17:06:56 +0530

    EOD-29

---
+++ Practice/PatternInverted.java
@@ -0,0 +1,18 @@
+public class PatternInverted {
+    public static void main(String args []){
+        int n=5;
+        // Outer Loop 
+        for( int i=1;i<=n;i++){
+            // inner loop -> Space print
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+        // inner loop -> star print
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+    }
+}
+ 
